4e701c99b6aa3c11e8d9acb31f417cc19f909d6f
Mathias Desloges <freakdev@gmail.com>
2018-01-22 14:39:24 +0100

    re-enforce control of input for webpack config cli api

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,24 @@ const defaultConfig = require( './webpack/config' )
 const fs = require('fs')
 const path = require('path')
 
+const knownEnv = ['dev', 'prod', 'test']
+
 module.exports = function (env, params) {
+
+    if (env === undefined) 
+        env = 'prod'
+
+    if (knownEnv.indexOf(env) === -1) 
+        throw new Error('Invalid environment')
+
     var configFile = path.resolve(__dirname, 'webpack/' + env + '.config.js')
-    var fstats = fs.statSync(configFile)
-    if (!fstats.isFile())
+    try {
+        var fstats = fs.statSync(configFile)
+    } catch (e) {
         throw new Error('Invalid config file or environment')
+    }
+    if (!fstats.isFile())
+        throw new Error('Invalid config file or environment')    
 
     var envConfig = require( configFile )
     return Object.assign(defaultConfig, envConfig)
